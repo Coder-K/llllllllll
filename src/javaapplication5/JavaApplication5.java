f8bedcb9188bc7511f828649692cd0308e6347f2
Coder-K <keachipane@gmail.com>
2021-08-11 17:50:50 +0200

    Java land

---
+++ src/javaapplication5/JavaApplication5.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication5;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author keamogetsoe chiapane
+ */
+public class JavaApplication5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        // TODO code application logic here
+        System.out.println("press one if you want to see");
+        int one = in.nextInt();
+        int num = 1 ;
+        if (one == num) {
+            System.out.println("i folled you my g");
+            
+        }
+        
+        
+    }
+    
+}
